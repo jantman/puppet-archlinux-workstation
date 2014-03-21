15c20d1ff51f53b71bc2a6fe2ae927f5d4755d0f
Jason Antman <Jason.Antman@coxinc.com>
2014-03-21 11:35:50 -0400

    fix perms inconsistency

--- files/maketmpdirs.sh
+++ files/maketmpdirs.sh
@@ -2,9 +2,9 @@
 ##############################################################
 # WARNING - WARNING - WARNING
 # This file is managed by the archlinux_workstation
-#  puppet module. Any local changes made here will be 
+#  puppet module. Any local changes made here will be
 #  overwritten on the next Puppet run. Please change the
 #  file in the puppet module for persistent changes.
 ##############################################################
 
-mkdir /tmp/sources ; chown root:wheel /tmp/sources ; chmod 0755 /tmp/sources
+mkdir /tmp/sources ; chown root:wheel /tmp/sources ; chmod 0775 /tmp/sources
