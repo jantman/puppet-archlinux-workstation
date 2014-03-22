372264af9dfc639382e635c14cd7338bfd9dbcfb
Jason Antman <Jason.Antman@coxinc.com>
2014-03-22 13:54:16 -0400

    fix non-root spec issues

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,9 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
+
+# per https://github.com/rodjek/rspec-puppet/issues/175
+RSpec.configure do |c|
+  c.before do
+    # avoid "Only root can execute commands as other users"
+    Puppet.features.stubs(:root? => true)
+  end
+end
