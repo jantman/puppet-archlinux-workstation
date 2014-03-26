3eb148c7cbdb7eeb43111012d1009869eecf7bf8
Jason Antman <Jason.Antman@coxinc.com>
2014-03-25 20:51:06 -0400

    puppet-lint fix

--- manifests/cups.pp
+++ manifests/cups.pp
@@ -10,15 +10,15 @@ class archlinux_workstation::cups {
 
   $cups_packages = ['libcups',
                     'cups',
-		    'ghostscript',
-		    'gsfonts',
-		    'gutenprint',
-		    'foomatic-db',
+                    'ghostscript',
+                    'gsfonts',
+                    'gutenprint',
+                    'foomatic-db',
 # @TODO: this is a package group, so it reinstalls on every run
-#		    'foomatic-filters',
-		    'hplip',
-		    'cups-pdf'
-		    ]
+#                    'foomatic-filters',
+                    'hplip',
+                    'cups-pdf'
+                    ]
 
   package {$cups_packages:
     ensure => present,
