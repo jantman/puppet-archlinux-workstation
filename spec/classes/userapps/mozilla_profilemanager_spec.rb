6259fd83074a6e9adb2bf47b5119e48172509ec5
Jason Antman <jason@jasonantman.com>
2014-03-25 20:43:38 -0400

    add mozilla profile manager via tarball of binaries, and staging module to deploy it

---
+++ spec/classes/userapps/mozilla_profilemanager_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe 'archlinux_workstation::userapps::googlechrome' do
+  context 'parameters' do
+    let(:facts) {{
+      :osfamily        => 'Archlinux',
+      :operatingsystem => 'Archlinux',
+    }}
+
+    let(:pre_condition) { "class {'archlinux_workstation::yaourt': }" }
+
+    describe "default" do
+      let(:params) {{ }}
+
+      it { should compile.with_all_deps }
+
+      it { should contain_staging_file('profilemanager.linux64.tar.gz').with({
+        'source' => 'ftp://ftp.mozilla.org/pub/mozilla.org/utilities/profilemanager/1.0/profilemanager.linux64.tar.gz',
+      }) }
+
+      it { should contain_staging_extract('profilemanager.linux64.tar.gz').with({
+        'target'  => '/usr/local/bin',
+        'creates' => '/usr/local/bin/profilemanager/profilemanager-bin',
+      }).that_requires('Staging::File[profilemanager.linux64.tar.gz]') }
+
+      it { should contain_file('/usr/share/applications/mozilla-profilemanager.desktop').with({
+        'ensure'  => 'present',
+        'source'  => 'puppet:///modules/archlinux_workstation/mozilla-profilemanager.desktop',
+      }).that_requires('Staging::Extract[profilemanager.linux64.tar.gz]') }
+
+    end
+
+  end
+
+end
