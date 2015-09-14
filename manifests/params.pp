602001b6b2d03cfaf4fdd2108a9ea3b1955dd358
Jason Antman <jason@jasonantman.com>
2015-09-14 13:09:39 -0400

    not using params class

--- manifests/params.pp
+++
@@ -1,8 +0,0 @@
-# == Class archlinux_workstation::params
-#
-# This class is meant to be called from archlinux_workstation
-# It sets variables according to platform
-#
-class archlinux_workstation::params {
-
-}
