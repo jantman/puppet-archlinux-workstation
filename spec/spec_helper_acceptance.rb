d56418f63442be36b2585445c2872f5dfa52ba1e
Jason Antman <jason@jasonantman.com>
2015-09-14 22:17:27 -0400

    acceptance tests working with a minor change to the helper

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -16,7 +16,7 @@ RSpec.configure do |c|
   # Configure all nodes in nodeset
   c.before :suite do
     # Install module and dependencies
-    puppet_module_install(:source => proj_root, :module_name => 'archlinux_workstation')
+    puppet_module_install(:source => proj_root, :module_name => 'archlinux_workstation', :target_module_path => '/etc/puppetlabs/code/modules')
     hosts.each do |host|
       on host, puppet('module', 'install', 'puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
     end
