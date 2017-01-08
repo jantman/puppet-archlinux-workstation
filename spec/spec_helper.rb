83064cc8322ce2c73b9b0dc04c949126919a31bc
Jason Antman <jason@jasonantman.com>
2017-01-08 09:17:11 -0500

    add some more facts that were missing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
+require 'puppet'
 
 # helper to allow easy definition of a base set of facts for all specs
 def spec_facts(additional = {})
@@ -7,6 +8,8 @@ def spec_facts(additional = {})
     :operatingsystem => 'Archlinux',
     :concat_basedir  => '/tmp',
     :processorcount  => 8,
+    :puppetversion   => Puppet::PUPPETVERSION,
+    :virtual         => 'physical',
     # structured facts
     :os              => { 'family' => 'Archlinux' },
     :processors      => { 'count' => 8 },
