64add6d8a839978092ca45937d5bfb9bed578783
Jason Antman <jason@jasonantman.com>
2017-12-26 09:48:44 -0500

    Get acceptance specs working with Vagrant/VBox; upgrade box

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -2,10 +2,6 @@ require 'beaker-rspec'
 require 'beaker/puppet_install_helper'
 require 'beaker/module_install_helper'
 
-run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
-install_module_on(hosts)
-install_module_dependencies_on(hosts)
-
 RSpec.configure do |c|
   # Project root
   proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
@@ -18,6 +14,9 @@ RSpec.configure do |c|
     # pacman update
     hosts.each do |h|
       on h, 'pacman -Syu --noconfirm'
+      run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
+      install_module_on(hosts)
+      install_module_dependencies_on(hosts)
     end
     # helper for spec/acceptance/classes/userapps/rvm_spec.rb
     scp_to(hosts, File.join(proj_root, 'spec', 'test_rvm.sh'), '/tmp/test_rvm.sh')
