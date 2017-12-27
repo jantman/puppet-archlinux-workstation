05a48d13870e43a4f9f096e2b9d8b4b54e4158da
Jason Antman <jason@jasonantman.com>
2017-12-27 13:08:51 -0500

    fix acceptance specs for move from vagrant/vbox to docker

--- spec/acceptance/classes/sudo_spec.rb
+++ spec/acceptance/classes/sudo_spec.rb
@@ -12,15 +12,11 @@ describe 'archlinux_workstation::sudo class' do
 
       # Run it twice and test for idempotency
       apply_manifest(pp, :catch_failures => true)
-      expect(apply_manifest(pp, :catch_failures => true).exit_code).to eq(2)
+      expect(apply_manifest(pp, :catch_failures => true).exit_code).to eq(0)
     end
 
     describe package('sudo') do
       it { should be_installed }
-    end
-
-    describe file('/etc/sudoers.d/11_vagrant-all') do
-      its(:content) { should match /vagrant ALL=\(ALL\) NOPASSWD: ALL/ }
     end
 
     describe file('/etc/sudoers.d/10_myuser-all') do
